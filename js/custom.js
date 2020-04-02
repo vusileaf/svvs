04a87e818dbc823e6abd85853afcedf2fc874c66
Neel Khalade <neel99khalade@gmail.com>
2020-04-02 14:18:36 +0530

    added custom file

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    function show_hide() {
+        var content = '.more_content';
+        var show_hide_button = '.show_hide';
+        $(content).hide();
+        $(show_hide_button).click(function() {
+            if($(content).is(":visible")) {
+                $(this).text("Show more");
+                $(content).hide();
+            } else {
+                $(this).text("Show less");
+                $(content).show();
+            }
+        });
+    }
+    show_hide();
+});
